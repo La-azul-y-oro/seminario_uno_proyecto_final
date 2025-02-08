8bd1984b8dad91163331394da20ad7f47e62b925
Gabriel Pereyra <gabrielp1689@gmail.com>
2025-02-08 19:38:40 -0300

    feat(CU-86b3gtpuw): add consortium model, service and controller

---
+++ .net/api/Models/Consortium.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace api.Models
+
+{
+    public class Consortium
+    {
+        public int Id { get; set; }
+
+        [StringLength(255)]
+        public required string Name { get; set; }
+
+        [StringLength(255)]
+        public required string Address { get; set; }
+        public bool Active { get; set; } = true;
+    }
+
+}
