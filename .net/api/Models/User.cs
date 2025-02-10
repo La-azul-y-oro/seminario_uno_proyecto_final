1fe915002a7bd4e11c25f2027f18701fafb73517
santiceron <ceronsanti@gmail.com>
2025-02-10 20:52:07 -0300

    feat(CU-86b3gtpzm): added User, DocumentType and Role to Models, added UserController

---
+++ .net/api/Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace api.Models
+{
+    public class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string lastName { get; set; }
+        public int documentNumber { get; set; }
+        public string telephoneNumber { get; set; }
+        public Role role { get; set; }
+        public DocumentType documentType { get; set; }
+    }
+}
