69d12436a43b9bf4936c3c7f6f35c0eac79badb4
santiceron <ceronsanti@gmail.com>
2025-02-11 16:40:51 -0300

    feat(CU-86b3gtpzm): updated User model and added DbSet for User in Context

--- .net/api/Models/User.cs
+++ .net/api/Models/User.cs
@@ -1,15 +1,29 @@
-﻿namespace api.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
 {
     public class User
     {
         public int id { get; set; }
-        public string name { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }
-        public string lastName { get; set; }
-        public int documentNumber { get; set; }
-        public string telephoneNumber { get; set; }
+
+        public required string name { get; set; }
+
+        public required string lastName { get; set; }
+
+        public required string email { get; set; }
+
+        public required string password { get; set; }
+
+        public required DocumentType documentType { get; set; }
+
+        [Column(TypeName = "BIGINT")]
+        public required long documentNumber { get; set; }
+
+        [Column(TypeName = "BIGINT")]
+        public long telephoneNumber { get; set; }
+
         public Role role { get; set; }
-        public DocumentType documentType { get; set; }
+
+        public bool active { get; set; } = true;
     }
 }
