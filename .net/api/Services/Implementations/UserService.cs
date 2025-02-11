4f270aa31cf94ad40c6fc866694d3282a1d9d748
santiceron <ceronsanti@gmail.com>
2025-02-11 13:30:33 -0300

    feat(CU-86b3gtpzm): added UserService

---
+++ .net/api/Services/Implementations/UserService.cs
@@ -0,0 +1,42 @@
+﻿using api.Context;
+using api.Models;
+using api.Services.Interfaces;
+
+namespace api.Services.Implementations
+{
+    public class UserService : IGenericService<User, int>
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(int id, User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(User entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
--- .net/api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ .net/api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ab517e74150b70439808ce5225656523c662aae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e25c321f998ce0f680f8cf86f659764520801a4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
