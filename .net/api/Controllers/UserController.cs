e25c321f998ce0f680f8cf86f659764520801a4e
santiceron <ceronsanti@gmail.com>
2025-02-11 13:11:17 -0300

    feat(CU-86b3gtpzm): added HTTP methods to UserController

--- .net/api/Controllers/UserController.cs
+++ .net/api/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using api.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -7,5 +8,49 @@ namespace api.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
+
+        public UserController() { }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetAll()
+        {
+            //UserService.GetAll()
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetById(int id)
+        {
+            //UserService.GetById()
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult<User> Create([FromBody] User user)
+        {
+            if(user == null) {
+                return BadRequest();
+            }
+
+            //UserService.create
+            return CreatedAtAction(nameof(GetById), new { id = user.id }, user);
+        }
+
+        [HttpPut]
+        public ActionResult<User> Update([FromBody] User user){
+            if(user == null)
+            {
+                return BadRequest();
+            }
+
+            // UserService.update
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id){
+            //UserService.delete
+            return Ok();
+        }
     }
 }
