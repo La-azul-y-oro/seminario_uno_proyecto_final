6ac54ede85397b5f713df8cf58462ab443cf3561
santiceron <ceronsanti@gmail.com>
2025-02-11 17:46:19 -0300

    feat(CU-86b3gtpzm): implemented HTTP methods in UserService

--- .net/api/Services/Implementations/UserService.cs
+++ .net/api/Services/Implementations/UserService.cs
@@ -1,6 +1,7 @@
 ﻿using api.Context;
 using api.Models;
 using api.Services.Interfaces;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace api.Services.Implementations
 {
@@ -15,27 +16,53 @@ namespace api.Services.Implementations
 
         public IEnumerable<User> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.User.Where(c => c.active).ToList();
         }
 
         public User GetById(int id)
         {
-            throw new NotImplementedException();
+            var user = _context.User.Find(id);
+            if(user == null || !user.active){ 
+                throw new KeyNotFoundException();
+            }
+
+            return user;
         }
 
         public void Update(int id, User entity)
         {
-            throw new NotImplementedException();
+            var user = _context.User.Find(id);
+
+            if(user == null || !user.active){
+                throw new KeyNotFoundException("User not found");
+            }
+
+            user.name = entity.name;
+            user.lastName = entity.lastName;
+            user.email = entity.email;
+            user.password = entity.password;
+            user.telephoneNumber = entity.telephoneNumber;
+
+            _context.SaveChanges();
         }
 
         public void Create(User entity)
         {
-            throw new NotImplementedException();
+            _context.User.Add(entity);
+            _context.SaveChanges();
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            var user = _context.User.Find(id);
+
+            if(user == null || !user.active)
+            {
+                throw new KeyNotFoundException("User not found");
+            }
+
+            user.active = false;
+            _context.SaveChanges();
         }
 
     }
