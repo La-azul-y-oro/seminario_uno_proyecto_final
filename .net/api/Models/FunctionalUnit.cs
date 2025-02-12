6144c0e82bd8ae80e3a3f5cc02256281cba1471e
santiceron <ceronsanti@gmail.com>
2025-02-12 16:35:38 -0300

    f:eat(CU-86b3gtpt8): added FunctionalUnit to Models

---
+++ .net/api/Models/FunctionalUnit.cs
@@ -0,0 +1,17 @@
+﻿namespace api.Models
+{
+    public class FunctionalUnit
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public long Balance { get; set; }
+
+        public long Factor { get; set; }
+
+        public int ConsortiumId { get; set; }
+
+        public bool Active { get; set; } = true;
+}
+}
