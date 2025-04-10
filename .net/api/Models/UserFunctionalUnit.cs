4b4efeecb56938195a6b99563b5c9efe0ac9c27e
santiceron <ceronsanti@gmail.com>
2025-04-10 18:49:37 -0300

    feat(CU-86b3y6w4x): created UserFunctionalUnit model and implemented intermediate table User/FunctionalUnit

---
+++ .net/api/Models/UserFunctionalUnit.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models
+{
+    [Table("user_functional_unit")]
+    public class UserFunctionalUnit
+    {
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+
+        public int FunctionalUnitId { get; set; }
+        public FunctionalUnit FunctionalUnit { get; set; } = null!;
+    }
+}
