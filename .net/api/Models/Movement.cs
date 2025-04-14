2e82ea3ccb36f3081e41ee19c0159c64ef6fd362
santiceron <ceronsanti@gmail.com>
2025-04-14 20:24:03 -0300

    feat(CU-86b3gtpub): created Movement model and Type enum

---
+++ .net/api/Models/Movement.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace api.Models
+{
+    public class Movement
+    {
+        public int id {  get; set; }
+        public required DateTime date { get; set; }
+        public required float amount { get; set; }
+
+        [JsonConverter(typeof(JsonStringEnumConverter))]
+        public required Type type { get; set; }
+        
+        [StringLength(255)]
+        public string receipt { get; set; }
+
+        [Column("consortium_id")]
+        public required int consortiumId { get; set; }
+
+        [Column("supplier_cuit")]
+        public required int supplierCuit{ get; set; }
+
+        [Column("concept_id")]
+        public required int conceptId { get; set; }
+
+        [Column("functional_unit_id")]
+        public required int functionalUnitId { get; set; }
+        public bool active { get; set; }
+
+    }
+}
---
+++ .net/api/Models/Type.cs
@@ -0,0 +1,8 @@
+﻿namespace api.Models
+{
+    public enum Type
+    {
+        INGRESO,
+        EGRESO
+    }
+}
